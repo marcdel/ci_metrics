866c7fe49c71f5cb5f7b30fb8b5889cf070a3239
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-09-16 09:04:12 -0700

    Add mix task to reprocess events

---
+++ lib/ci_metrics/tasks/reprocess_events.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.ReprocessEvents do
+  use Mix.Task
+
+  @shortdoc "Reprocesses all events to create the associated objects."
+  def run(_) do
+    Mix.Task.run("app.start")
+
+    CiMetrics.Project.Event.get_all()
+    |> Enum.map(&CiMetrics.Project.process_event/1)
+  end
+end
