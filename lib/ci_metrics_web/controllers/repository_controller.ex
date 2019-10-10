5a391dd5219ef3ae0a0b0d7555263880d2352060
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-10-09 20:52:32 -0700

    Use time unit metric to display lead time in a user friendly way

--- lib/ci_metrics_web/controllers/repository_controller.ex
+++ lib/ci_metrics_web/controllers/repository_controller.ex
@@ -1,15 +1,20 @@
 defmodule CiMetricsWeb.RepositoryController do
   use CiMetricsWeb, :controller
   alias CiMetrics.GithubProject
+  alias CiMetrics.Metrics.TimeUnitMetric
   alias CiMetrics.Project.Repository
 
   @project Application.get_env(:ci_metrics, :project, GithubProject)
 
   def show(conn, params) do
-    {average_lead_time, :minutes} = @project.calculate_lead_time(params["id"])
+    lead_time =
+      params["id"]
+      |> @project.calculate_lead_time()
+      |> TimeUnitMetric.to_string()
+
     %{owner: owner, name: name} = Repository.get(params["id"])
 
-    repo = %{owner: owner, name: name, average_lead_time: average_lead_time}
+    repo = %{owner: owner, name: name, average_lead_time: lead_time}
     render(conn, "show.html", repository: repo)
   end
 end
