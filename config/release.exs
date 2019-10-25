faf8a427da776659ae58521cb4b427ed7c59152a
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-10-24 20:42:07 -0700

    Setup for gigalixir

---
+++ config/release.exs
@@ -0,0 +1,4 @@
+config :ci_metrics, CiMetricsWeb.Endpoint,
+       server: true,
+       http: [port: {:system, "PORT"}],
+       url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
